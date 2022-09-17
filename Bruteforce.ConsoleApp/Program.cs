9d1e5b9eb4ef5718e874f6cb7d85fa80a22d0461
Stepan Sidak <ssidak@netfully.com>
2022-09-17 21:18:35 +0300

    Add project files.

---
+++ Bruteforce.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bruteforce.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
